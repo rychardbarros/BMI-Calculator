b366b19d53fa65abe44fe19430f3c41a75669038
RycharDev <rychard.barros.dev@gmail.com>
2023-10-27 01:14:19 -0300

    Update index.js

--- assets/index.js
+++ assets/index.js
@@ -18,32 +18,26 @@ form.addEventListener('submit', function(ev) {
         const imc = weight / (height * height);
 
         if (imc < 18.5) {
-            classificacao = 'abaixo do peso';
             result.style.backgroundColor = 'red';
             result.style.color = 'white'
             result.innerHTML = `Ola ${name} seu IMC é: ${imc.toFixed(2)} - Você está abaixo do peso.`;
         } else if (imc < 25) {
-            classificacao = 'com peso ideal. Parabéns!!!';
             result.style.backgroundColor = 'green';
             result.style.color = 'white'
             result.innerHTML = `Ola ${name} seu IMC é: ${imc.toFixed(2)} - Você está com peso ideal. Parabéns!!!`;
         } else if (imc < 30) {
-            classificacao = 'levemente acima do peso.';
             result.style.backgroundColor = 'yellow';
             result.style.color = 'black'
             result.innerHTML = `Ola ${name} seu IMC é: ${imc.toFixed(2)} - Você está levemente acima do peso.`;
         } else if (imc < 35) {
-            classificacao = 'com obesidade grau I.';
             result.style.backgroundColor = 'orange';
             result.style.color = 'black'
             result.innerHTML = `Ola ${name} seu IMC é: ${imc.toFixed(2)} - Você está com obesidade grau I.`;
         } else if (imc < 40) {
-            classificacao = 'com obesidade grau II';
             result.style.backgroundColor = 'red';
             result.style.color = 'white'
             result.innerHTML = `Ola ${name} seu IMC é: ${imc.toFixed(2)} - Você está com obesidade grau II.`;
         } else {
-            classificacao = 'com obesidade grau III. Cuidado!!';
             result.style.backgroundColor = 'darkred';
             result.style.color = 'white'
             result.innerHTML = `Ola ${name} seu IMC é: ${imc.toFixed(2)} - Você está com obesidade grau III. Cuidado!!`;
