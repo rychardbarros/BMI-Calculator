1c2cbdda36e103b3789cdb9a4ebf408b2456daf4
RycharDev <rychard.barros.dev@gmail.com>
2023-10-26 23:05:55 -0300

    Update index.js

--- assets/index.js
+++ assets/index.js
@@ -8,33 +8,45 @@ form.addEventListener('submit', function(ev) {
     const weightInput = document.getElementById('weight').value;
     const heightInput = document.getElementById('height').value;
 
-    const weight = Number(weightInput.replace(/,/g, '').replace('.', ''));
-    const height = Number(heightInput.replace(/,/g, '').replace('.', ''));
-
-    console.log(name, weight, height);
+    const weight = parseFloat(weightInput.replace(/,/g, '').replace('.', ''));
+    const height = parseFloat(heightInput.replace(/,/g, '').replace('.', '') / 100);
 
     if (name.trim() === '' || isNaN(weight) || isNaN(height) || weight <= 0 || height <= 0) {
         alert('Por favor, preencha todos os campos com valores válidos.');
     } else {
         // Validação bem-sucedida, faça o cálculo do IMC aqui
-        const imc = (weight / height ** 2).toFixed(5);
-        
-        let classificacao = '';
+        const imc = weight / (height * height);
 
-        if (imc < 0.0018,5) {
-            classificacao = 'abaixo do peso.';
-        } else if (imc < 0.0025) {
+        if (imc < 18.5) {
+            classificacao = 'abaixo do peso';
+            result.style.backgroundColor = 'red';
+            result.style.color = 'white'
+            result.innerHTML = `Ola ${name} seu IMC é: ${imc.toFixed(2)} - Você está abaixo do peso.`;
+        } else if (imc < 25) {
             classificacao = 'com peso ideal. Parabéns!!!';
-        } else if (imc < 0.0030) {
+            result.style.backgroundColor = 'green';
+            result.style.color = 'white'
+            result.innerHTML = `Ola ${name} seu IMC é: ${imc.toFixed(2)} - Você está com peso ideal. Parabéns!!!`;
+        } else if (imc < 30) {
             classificacao = 'levemente acima do peso.';
-        } else if (imc < 0.0035) {
+            result.style.backgroundColor = 'yellow';
+            result.style.color = 'white'
+            result.innerHTML = `Ola ${name} seu IMC é: ${imc.toFixed(2)} - Você está levemente acima do peso.`;
+        } else if (imc < 35) {
             classificacao = 'com obesidade grau I.';
-        } else if (imc < 0.0040) {
+            result.style.backgroundColor = 'orange';
+            result.style.color = 'white'
+            result.innerHTML = `Ola ${name} seu IMC é: ${imc.toFixed(2)} - Você está com obesidade grau I.`;
+        } else if (imc < 40) {
             classificacao = 'com obesidade grau II';
+            result.style.backgroundColor = 'red';
+            result.style.color = 'white'
+            result.innerHTML = `Ola ${name} seu IMC é: ${imc.toFixed(2)} - Você está com obesidade grau II.`;
         } else {
             classificacao = 'com obesidade grau III. Cuidado!!';
+            result.style.backgroundColor = 'darkred';
+            result.style.color = 'white'
+            result.innerHTML = `Ola ${name} seu IMC é: ${imc.toFixed(2)} - Você está com obesidade grau III. Cuidado!!`;
         }
-
-        alert(`Seu IMC é: ${imc} e você está ${classificacao}`);
     }
 });
